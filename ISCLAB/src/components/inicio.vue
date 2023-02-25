752dcfd43d1b4ec17aef980f7de3f3875f447b67
josue-canul <josue.19070015@itsmotul.edu.mx>
2023-02-25 16:25:32 -0600

    c1

---
+++ ISCLAB/src/components/inicio.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-container>
+
+        <v-layout row wrap>
+
+            <v-flex>
+
+                <v-card class="carta">
+
+                   <v-card-title>
+                        <h1>
+                            ¿QUE HACEMOS EN ISCLAB?
+                        </h1>
+                   </v-card-title>
+                        <br>
+                   <v-card-text>
+                        <H4>
+                            <p>  Desarrollamos aplicaciones con fines de práctica, investigación y responsabilidad social.
+
+                    Buscamos el desarrollo de nuestros estudiantes a través de la creación de proyectos que sean útiles a la sociedad.
+
+                    ¿Tienes un proyecto en mente? Compártelo con nosotros y buscaremos la manera de ayudarte.</p>
+                        </H4>
+                   </v-card-text>
+
+                </v-card>
+            </v-flex>
+            <br>
+            <br>
+            <v-flex>
+
+                <v-card>
+
+                     <v-card-title>
+                        <h1>
+                            ¿PUEDEN CREAR UN SITIO WEB GRATUITO PARA MI PROYECTO?
+                        </h1>
+                    </v-card-title>
+                        <br>
+                    <v-card-text>
+                        <H4>
+                            <p>La respuesta corta es sí.
+
+Claro que necesitamos saber en qué consiste el sitio que necesitas y tu historia para valorar el apoyo gratuito en la creación y publicación de tu sitio.
+
+Averigua las condiciones de un sitio web gratuito para nosotros será un placer apoyarte.</p>
+                        </H4>
+                    </v-card-text>
+
+                </v-card>
+            </v-flex>
+
+
+        </v-layout>
+
+    </v-container>
+
+</template>
+<style>
+    h1 {
+        color: rgb(135, 183, 202);
+    }
+
+    h4 {
+        color:bisque
+    }
+</style>
